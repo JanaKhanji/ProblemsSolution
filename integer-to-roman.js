904a9dd6ae46abf22749e96a229e01d852dee0d8
jkhanji <jkhanji@turning.com>
2023-05-04 08:56:40 +0300

    Solve int to roman

--- integer-to-roman.js
+++ integer-to-roman.js
@@ -2,48 +2,52 @@
 // Input: num = 58
 // Output: "LVIII"
 
-// Not working yet
 var intToRoman = function (num) {
-    const iteration = num / 5;
-    let rest = num % 5;
-    const roman = [];
-    console.log(iteration, rest);
-    if (iteration > 1) {
-      let temp = iteration % 10;
-      console.log(intToRomanChar((iteration - temp) * 5));
-      roman.push(intToRomanChar((iteration - temp) * 5));
-      // roman.push(intToRoman(5 * temp));
-    } else {
+  let roman = [];
+  let array = num.toString().split("");
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    const element = array[array.length - i - 1];
+    let rest = element % 5;
+    if (rest >= 1 && rest <= 3) {
       while (rest > 0) {
-        if (intToRomanChar(rest) !== 0) {
-          roman.push(intToRomanChar(rest));
-          break;
-        } else {
-          roman.push("I");
-          rest--;
-        }
+        roman.unshift(intToRomanChar(10 ** i));
+        rest--;
       }
+      if (element > 5) {
+        roman.unshift(intToRomanChar(5 * 10 ** i));
+      }
+    } else if (rest === 4) {
+      if (element > 5) {
+        roman.unshift(intToRomanChar(10 ** (i + 1)));
+      } else {
+        roman.unshift(intToRomanChar(5 * 10 ** i));
+      }
+      roman.unshift(intToRomanChar(10 ** i));
+    } else {
+      roman.unshift(intToRomanChar(+element * 10 ** i));
     }
-    return roman;
+  }
+  return roman.join("");
 };
-  
+
 var intToRomanChar = function (c) {
-    switch (c) {
-        case 1:
-        return "I";
-        case 5:
-        return "V";
-        case 10:
-        return "X";
-        case 50:
-        return "L";
-        case 100:
-        return "C";
-        case 500:
-        return "D";
-        case 1000:
-        return "M";
-        default:
-        return null;
-    }
+  switch (c) {
+    case 1:
+      return "I";
+    case 5:
+      return "V";
+    case 10:
+      return "X";
+    case 50:
+      return "L";
+    case 100:
+      return "C";
+    case 500:
+      return "D";
+    case 1000:
+      return "M";
+    default:
+      return null;
+  }
 };
