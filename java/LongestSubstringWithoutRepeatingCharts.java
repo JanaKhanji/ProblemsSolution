e9d3777de6a6a489ff28081ca644fed40557d212
JanaKhanji <janakhanji98@gmail.com>
2026-01-14 12:26:06 +0200

    Solve LongestSubstringWithoutRepeatingCharts

--- java/LongestSubstringWithoutRepeatingCharts.java
+++ java/LongestSubstringWithoutRepeatingCharts.java
@@ -1,6 +1,27 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-// TODO
+
 class LongestSubstringWithoutRepeatingCharts {
-    
+     public static int getLengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        String longestSubString = "";
+        int lengthWithNoRepeat = 0;
+        for(int i = 0; i < s.length(); i++) {
+                if (longestSubString.contains(s.charAt(i) + "")) {
+                    longestSubString = longestSubString.substring(longestSubString.indexOf(s.charAt(i)) + 1);
+                }
+                longestSubString += s.charAt(i);
+                lengthWithNoRepeat = Math.max(lengthWithNoRepeat, longestSubString.length());
+        }
+        return lengthWithNoRepeat;
+    }
+
+    public static void main(String[] args) {
+    //    String s = "abcabcbb";
+       // String s = "bbbbb";
+        // String s = "pwwkew";
+        String s = "aab";
+        // String s = "dvdf";
+        System.out.println(getLengthOfLongestSubstring(s));
+    }
 }
---
+++ java/LowestCommonAncestorBinarySearchTree.java
@@ -0,0 +1,3 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+// TODO
